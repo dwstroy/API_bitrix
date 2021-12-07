0f83cdd0ad22c4a09758ff2e663a669f8504507b
dwstroy <denis@dwstroy.ru>
2021-12-07 22:12:05 +0300

    v1.0 bMenu

---
+++ IncludeComponent/bMenu/.bottom1.menu.php
@@ -0,0 +1,60 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Туры в Иран зимой", 
+		"/tury_v_iran_zimoy/", 
+		Array("active"), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в декабре", 
+		"/tury_v_iran_v_dekabre/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в январе", 
+		"/tury_v_iran_v_yanvare/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в феврале", 
+		"/tury_v_iran_v_fevrale/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в весной", 
+		"/tury_v_iran_v_vesnoy/", 
+		Array("active", "mtop"), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в марте", 
+		"/tury_v_iran_v_marte/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в апреле", 
+		"/tury_v_iran_v_aprele/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Туры в Иран в мае", 
+		"/tury_v_iran_v_mae/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ IncludeComponent/bMenu/bitrix_menu.php
@@ -0,0 +1,16 @@
+<?$APPLICATION->IncludeComponent("bitrix:menu", "dws_footer_nav", Array(
+    "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+    "CHILD_MENU_TYPE" => "bottom1",	// Тип меню для остальных уровней
+    "DELAY" => "N",	// Откладывать выполнение шаблона меню
+    "MAX_LEVEL" => "1",	// Уровень вложенности меню
+    "MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+        0 => "",
+    ),
+    "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+    "MENU_CACHE_TYPE" => "N",	// Тип кеширования
+    "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+    "ROOT_MENU_TYPE" => "bottom1",	// Тип меню для первого уровня
+    "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+),
+    false
+);?>
